f7ad3f40e62e34e9f7ffc3b96d4017a3f8d70cd2
Derek Rayside <drayside@uwaterloo.ca>
2013-06-02 09:15:53 -0400

    pre-release of source files for lab5

--- src/ece351/w/parboiled/WParboiledRecognizer.java
+++ src/ece351/w/parboiled/WParboiledRecognizer.java
@@ -1,66 +1,64 @@
 package ece351.w.parboiled;
-import java.io.File;
-
 import org.parboiled.Rule;
 import org.parboiled.annotations.BuildParseTree;
 import org.parboiled.common.FileUtils;
-import org.parboiled.common.ImmutableList;
 
 import ece351.util.BaseParser351;
 import ece351.w.ast.WProgram;
-import ece351.w.ast.Waveform;
+
 
 @BuildParseTree
 //Parboiled requires that this class not be final
-public /*final*/ class WParboiledParser extends BaseParser351 {
+public /*final*/ class WParboiledRecognizer extends BaseParser351 {
 
 	/**
-	 * Run this parser, exit with error code 1 for malformed input.
-	 * Called by wave/Makefile.
-	 * @param args
+	 * Run this recognizer, exit with error code 1 to reject.
+	 * This method is called by wave/Makefile.
+	 * @param args args[0] is the name of the input file to read
 	 */
 	public static void main(final String[] args) {
-    	process(WParboiledParser.class, FileUtils.readAllText(args[0]));
+    	process(WParboiledRecognizer.class, FileUtils.readAllText(args[0]));
     }
+	
+	public static void recognize(final String inputText) {
+		process(WParboiledRecognizer.class, inputText);
+	}
 
-	/**
-	 * Construct an AST for a W program. Use this for debugging.
+	
+	/** 
+	 * Use this method to print the parse tree for debugging.
+	 * @param w the text of the W program to recognize
 	 */
-	public static WProgram parse(final String inputText) {
-		return (WProgram) process(WParboiledParser.class, inputText).resultValue;
+	public static void printParseTree(final String w) {
+		printParseTree(WParboiledRecognizer.class, w);
 	}
 
 	/**
 	 * By convention we name the top production in the grammar "Program".
 	 */
 	@Override
-    public Rule Program() {
-        		// push empty WProgram
-// TODO: 5 lines snipped
+	public Rule Program() {
+// TODO: 1 lines snipped
 throw new ece351.util.Todo351Exception();
-    }
-
+	}
+    
 	/**
 	 * Each line of the input W file represents a "pin" in the circuit.
 	 */
     public Rule Waveform() {
-    			// peek() == [pin name, WProgram]
-    			// push empty Waveform with name
-    			// peek() = [Waveform, WProgram]
-    			// swap, pop, append, push
-    			// peek() = [WProgram]
-// TODO: 18 lines snipped
+// TODO: 1 lines snipped
 throw new ece351.util.Todo351Exception();
     }
 
     /**
-     * The first token on each line is the name of the pin that line represents.
+     * The first token in each statement is the name of the waveform 
+     * that statement represents.
      */
     public Rule Name() {
-// TODO: 4 lines snipped
+// TODO: 1 lines snipped
 throw new ece351.util.Todo351Exception();
     }
-    
+
     /**
      * A Name is composed of a sequence of Letters. 
      * Recall that PEGs incorporate lexing into the parser.
@@ -82,9 +80,8 @@ throw new ece351.util.Todo351Exception();
      * A BitString is composed of a sequence of Bits. 
      * Recall that PEGs incorporate lexing into the parser.
      */
-    public Rule Bit() {   
-        		// peek() = [Waveform, WProgram]
-// TODO: 5 lines snipped
+    public Rule Bit() {       
+// TODO: 1 lines snipped
 throw new ece351.util.Todo351Exception();
     }
 
