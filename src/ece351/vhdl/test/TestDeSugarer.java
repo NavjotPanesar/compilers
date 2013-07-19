2b8b81dab0bbdf5589f07756b852bbb0033447af
Derek Rayside <drayside@uwaterloo.ca>
2013-07-19 14:57:15 -0400

    updating TestDeSugarer to check that exotic operators have been removed

--- src/ece351/vhdl/test/TestDeSugarer.java
+++ src/ece351/vhdl/test/TestDeSugarer.java
@@ -10,11 +10,25 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import ece351.TestPrelab;
+import ece351.common.ast.AndExpr;
+import ece351.common.ast.ConstantExpr;
+import ece351.common.ast.EqualExpr;
+import ece351.common.ast.Expr;
+import ece351.common.ast.NAndExpr;
+import ece351.common.ast.NOrExpr;
+import ece351.common.ast.NaryAndExpr;
+import ece351.common.ast.NaryOrExpr;
+import ece351.common.ast.NotExpr;
+import ece351.common.ast.OrExpr;
+import ece351.common.ast.VarExpr;
+import ece351.common.ast.XNOrExpr;
+import ece351.common.ast.XOrExpr;
 import ece351.util.CommandLine;
 import ece351.util.ExaminableProperties;
 import ece351.util.TestInputs351;
 import ece351.vhdl.DeSugarer;
 import ece351.vhdl.VParser;
+import ece351.vhdl.ast.PostOrderVExprVisitor;
 import ece351.vhdl.ast.VProgram;
 
 
@@ -33,7 +47,7 @@ public final class TestDeSugarer {
 	}
 
 	@Test
-	public void parse() {
+	public void desugar() {
 		assertTrue(TestPrelab.areAssertionsEnabled());
 
 		final String inputSpec = f.getAbsolutePath();
@@ -46,6 +60,11 @@ public final class TestDeSugarer {
 		final String pp = vp1.toString();
 		System.out.println("desugared program: ");
 		System.out.println(pp);
+
+		// check the desugared program does not contain any exotic operators
+		final DesugarCheck dsc = new DesugarCheck();
+		// this will thrown an exception if the tree is not desugared
+		dsc.traverse(vp1);
 		
 		// find the appropriate solution file for comparison
 		String solnSpec = "";
@@ -76,4 +95,22 @@ public final class TestDeSugarer {
 		System.out.println("accepted, as expected:  " + inputSpec);
 	}
 
+	private final static class DesugarCheck extends PostOrderVExprVisitor {
+
+		// a desugared tree should not contain these
+		@Override public Expr visit(XOrExpr e) { throw new IllegalStateException("xor not desugared"); } 
+		@Override public Expr visit(EqualExpr e) { throw new IllegalStateException("EqualExpr not desugared"); }
+		@Override public Expr visit(NAndExpr e) { throw new IllegalStateException("nand not desugared"); }
+		@Override public Expr visit(NOrExpr e) { throw new IllegalStateException("nor not desugared"); }
+		@Override public Expr visit(XNOrExpr e) { throw new IllegalStateException("xnor not desugared"); }
+		
+		// these are ok in the desugared tree
+		@Override public Expr visit(final ConstantExpr e) { return e; }
+		@Override public Expr visit(final VarExpr e) { return e; }
+		@Override public Expr visit(final NotExpr e) { return e; }
+		@Override public Expr visit(final AndExpr e) { return e; }
+		@Override public Expr visit(final OrExpr e) { return e; }
+		@Override public Expr visit(final NaryAndExpr e) { return e; }
+		@Override public Expr visit(final NaryOrExpr e) { return e; }
+	}
 }
